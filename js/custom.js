8a8e983cbfb098c8deea0393ca7aa75c0799b313
vivante <gauravkdma@gmail.com>
2019-06-13 20:02:09 +0530

    added newsbox

--- js/custom.js
+++ js/custom.js
@@ -238,3 +238,17 @@ $(document).ready(function()
 	}
 
 });
+
+
+
+
+// // START SCRIPT
+
+// // START SCROLLING NEWS
+// document.write('<div id="news_iframe_scroll">');
+// document.write('<div class="news_scroll-title">');
+// document.write('News and Updates<br>');
+// document.write('</div>');
+// document.write('<iframe name="NewsIFrame" src="news_scroll.html" frameborder="0" scrolling="no"></iframe>');
+// document.write('</div>');
+// // END SCROLLING NEWS
